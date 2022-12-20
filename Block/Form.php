c0b1684cf1a0798774cf2de5b0961e4db9e665de
Salvatore Pruiti <salvatorepruiti@hotmail.com>
2022-12-20 11:05:12 +0100

    Aggiunto Track Reso Tipi di Reso e Brand vengono adesso letti dal MW

--- Block/Form.php
+++ Block/Form.php
@@ -3,9 +3,24 @@
 namespace ScaliaGroup\AssistanceSystem\Block;
 
 use Magento\Framework\View\Element\Template;
+use ScaliaGroup\Integration\Helper\Config;
+use Laminas\Http\Client;
 
 class Form extends Template
 {
+    protected Config $config;
+
+    public function __construct(
+        Template\Context $context,
+        Config $config,
+        array $data = []
+    )
+    {
+        parent::__construct($context, $data);
+        $this->config = $config;
+    }
+
+
     public function getFormAction()
     {
         return $this->getUrl('assistenza', ['_secure' => true]);
@@ -28,22 +43,65 @@ class Form extends Template
 
     public function getBrands()
     {
-        return [
-            'Piquadro',
-            'Pollini',
-            'The Bridge',
-            'Coccinelle',
-            'Travelite',
-            'Titan'
-        ];
+        try {
+            $host = $this->config->getMiddlewareHost();
+            $access_token = $this->config->getMiddlewareAccessToken();
+
+            $client = new Client($host . "/api/v1/returns/brands");
+
+            $client->setHeaders([
+                'Accept' => 'application/json',
+                'Authorization' => 'Bearer ' . $access_token
+            ]);
+
+            $client->setMethod("get");
+            $response = $client->send();
+
+            if($response->getStatusCode() != 200)
+                throw new \Exception($response->getStatusCode() . ' ' . $response->getReasonPhrase());
+
+            return json_decode($response->getBody(), true);
+
+        } catch (\Exception $e) {
+
+            return [
+
+            ];
+
+        }
+
+
     }
 
     public function getTypes()
     {
-        return [
-            1 => 'Bagaglio DAT danneggiato',
-            2 => 'Prodotto difettoso in garanzia',
-            3 => 'Riparazione a pagamento'
-        ];
+        try {
+            $host = $this->config->getMiddlewareHost();
+            $access_token = $this->config->getMiddlewareAccessToken();
+
+            $client = new Client($host . "/api/v1/returns/types");
+
+            $client->setHeaders([
+                'Accept' => 'application/json',
+                'Authorization' => 'Bearer ' . $access_token
+            ]);
+
+            $client->setMethod("get");
+            $response = $client->send();
+
+            if($response->getStatusCode() != 200)
+                throw new \Exception($response->getStatusCode() . ' ' . $response->getReasonPhrase());
+
+            return json_decode($response->getBody(), true);
+
+        } catch (\Exception $e) {
+
+            return [
+                1 => 'Bagaglio DAT danneggiato',
+                2 => 'Prodotto difettoso in garanzia',
+                3 => 'Riparazione a pagamento'
+            ];
+
+        }
     }
 }
